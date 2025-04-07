a170240f2422eb34554ca724c5a94260c07917b0
MarkHuynh <popmusicfan1992@gmail.com>
2025-04-07 22:51:00 +0700

    update

--- shopping-cart/vite.config.js
+++ shopping-cart/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Đặt base phù hợp với URL deploy của bạn
 export default defineConfig({
   plugins: [react()],
+  base: '/react-project/' // Thay đổi "react-project" theo tên repository của bạn
 })
