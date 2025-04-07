01d131e58253aad825642a24ad8c08c32e5ea5f3
MarkHuynh <popmusicfan1992@gmail.com>
2025-04-07 23:25:05 +0700

    add

--- shopping-cart/vite.config.js
+++ shopping-cart/vite.config.js
@@ -1,8 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // Đặt base phù hợp với URL deploy của bạn
 export default defineConfig({
-  plugins: [react()],
-  base: '/react-project/' // Thay đổi "react-project" theo tên repository của bạn
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'My React App',
+        short_name: 'ReactApp',
+        start_url: '.',
+        display: 'standalone',
+        background_color: '#ffffff',
+        icons: [
+          {
+            src: '/logo.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/logo.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
+  base: '/react-project/shopping-cart/' // Thay đổi "react-project" theo tên repository của bạn
 })
